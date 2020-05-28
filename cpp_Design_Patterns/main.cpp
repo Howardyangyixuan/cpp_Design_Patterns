c51d4ce09e161624ac6b7ebb14704c75adb328bd
howardyangyixuan <howardyangyixuan@gmail.com>
2020-05-28 18:21:22 +0800

    Initial Commit

---
+++ cpp_Design_Patterns/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp_Design_Patterns
+//
+//  Created by 杨义轩 on 2020/5/28.
+//  Copyright © 2020 杨义轩. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
